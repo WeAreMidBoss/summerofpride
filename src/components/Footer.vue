f95db43f8b7af2f75e3d99a33bb4ef92158bce33
Twinlurker <twinlurker@gmail.com>
2020-04-20 02:47:50 -0700

    Updating lost changes

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-12">
+          <p>&copy; Copyright <span id="year"></span><br>
+           MidBoss, LLC.<br>
+          </p>
+        </div>
+        <div class="col-md-4 col-sm-12">
+          <div class="social-links justify-content-end">
+            <a href="https://twitter.com/MidBoss" target="_blank"><img src="imgs/Twitter.svg" alt="Twitter" /></a>
+            <a href="https://www.facebook.com/WeAreMidBoss" target="_blank"><img src="imgs/Facebook.svg" alt="Facebook" /></a>
+            <a href="http://midboss.com/press" target="_blank"><img src="imgs/PressKit.svg" alt="Press Kit" /></a>
+            <a href="privacy.html" target="_blank"><img src="imgs/PrivacyPolicy.svg" alt="Privacy Policy" /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
