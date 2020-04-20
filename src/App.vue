f48a0a794828ba1351681fb2311a63f9141fbbca
Twinlurker <twinlurker@gmail.com>
2020-04-20 03:59:13 -0700

    Updated streamers info

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
                   <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
                     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_56b65e1f2b3ba5e696ffa9e29_748c34fc41" tabindex="-1" value=""></div>
-                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
                     </div>
                 </form>
                 </div>
