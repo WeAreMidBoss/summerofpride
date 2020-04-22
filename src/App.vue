f48e1f745be5b96bd82ef74f2bb86fec3da26336
Twinlurker <twinlurker@gmail.com>
2020-04-22 04:41:43 -0700

    Changes styles for mailing button

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
                 <form action="https://midboss.us5.list-manage.com/subscribe/post?u=56b65e1f2b3ba5e696ffa9e29&amp;id=748c34fc41" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                     <div id="mc_embed_signup_scroll">
                   <label for="mce-EMAIL"><h3>Stay Up-to-Date On the Event!</h3></label>
-                  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
                     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_56b65e1f2b3ba5e696ffa9e29_748c34fc41" tabindex="-1" value=""></div>
                     <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
