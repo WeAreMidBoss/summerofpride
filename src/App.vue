5e45cc033a6ae96c88bba21c9d200353d7d8a77f
Kossio <skossio@gmail.com>
2021-07-29 02:46:47 -0700

    Added new charities supporting the Activision Blizzard Walkout

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,23 @@
               <p class="align-middle" data-aos="slide-up">The Season of Pride is a month-long event of LGBTQIA+ (queer) streamers playing queer games, throughout the month of July. 
                     These fun “gaymers” will be playing some of the best and most unique games that showcase positive representation of queer characters and stories. 
                     Tune in and discover some really great games, and follow some really fun streamer personalities.</p>
-              <p data-aos="slide-up">This year, we’re also supporting three wonderful charities—<a href="https://www.thetrevorproject.org/" target="_blank">The Trevor Project</a>, <a href="https://www.translifeline.org/" target="_blank">Trans Lifeline</a> and <a href="https://gaymerx.org/" target="_blank">GaymerX</a>. Tune into any stream and share your support if you can.</p>
+              <p data-aos="slide-up">This year, we’re also supporting eight wonderful charities—<a href="https://www.thetrevorproject.org/" target="_blank">The Trevor Project</a>, 
+              <a href="https://www.translifeline.org/" target="_blank">Trans Lifeline</a>, <a href="https://gaymerx.org/" target="_blank">GaymerX</a>, 
+              <a href="https://mermaidsuk.org.uk/" target="_target">Mermaids</a>, <a href="https://www.minus18.org.au/" target="_target">Minus18</a>, 
+              <a href="https://www.blackgirlscode.com/" target="_target">Black Girls Code</a>, <a href="https://www.getwigi.com/" target="_target">Women In Games International</a>, 
+              and <a href="https://girlswhocode.com/" target="_target">Girls Who Code</a>.</p> 
+              
+              <p data-aos="slide-up">Tune into any stream and share your support if you can.</p>
               
               <div align="center" class="charities row">
                 <div class="charity col"><div><a href="https://gaymerx.org/" target="_target"><img class="" src="./assets/img/gaymerx.png" alt="GaymerX" /></a></div> <div align="center" class="donate"><a href="https://donate.tiltify.com/+season-of-pride-2021/gaymerx-2021" target="_target">DONATE</a></div></div> 
                 <div class="charity col"><div><a href="https://www.thetrevorproject.org/" target="_target"><img class="" src="./assets/img/trevorproject.png" alt="The Trevor Project" /></a></div> <div class="donate"><a href="https://donate.tiltify.com/+season-of-pride-2021/trevor-project-2021" target="_target">DONATE</a></div></div> 
                 <div class="charity col"><div><a href="https://www.translifeline.org/" target="_target"><img class="" src="./assets/img/translifeline.png" alt="Trans Lifeline" /></a></div> <div class="donate"><a href="https://donate.tiltify.com/+season-of-pride-2021/trans-lifeline-2021" target="_target">DONATE</a></div></div> 
+                <div class="charity col"><div><a href="https://mermaidsuk.org.uk/" target="_target"><img class="" src="./assets/img/mermaids.png" alt="Mermaids" /></a></div> <div align="center" class="donate"><a href="https://donate.tiltify.com/+season-of-pride-2021/mermaids-2021" target="_target">DONATE</a></div></div> 
+                <div class="charity col"><div><a href="https://www.minus18.org.au/" target="_target"><img class="" src="./assets/img/minus18.png" alt="Minus 18" /></a></div> <div class="donate"><a href="https://donate.tiltify.com/+season-of-pride-2021/minus18-2021" target="_target">DONATE</a></div></div> 
+                <div class="charity col"><div><a href="https://www.blackgirlscode.com/" target="_target"><img class="" src="./assets/img/blackgirlscode.png" alt="Black Girls Code" /></a></div> <div class="donate"><a href="https://donate.tiltify.com/+season-of-pride-2021/black-girls-code-2021" target="_target">DONATE</a></div></div> 
+                <div class="charity col"><div><a href="https://www.getwigi.com/" target="_target"><img class="" src="./assets/img/wigi.png" alt="Womein In Games International" /></a></div> <div class="donate"><a href="https://donate.tiltify.com/+season-of-pride-2021/women-in-games-international-2021" target="_target">DONATE</a></div></div> 
+                <div class="charity col"><div><a href="https://girlswhocode.com/" target="_target"><img class="" src="./assets/img/girlswhocode.png" alt="Girls Who Code" /></a></div> <div align="center" class="donate"><a href="https://donate.tiltify.com/+season-of-pride-2021/girls-who-code-2021" target="_target">DONATE</a></div></div> 
               </div>
               <hr>
               <!-- Begin MailChimp Signup Form -->
